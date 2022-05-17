97093d1c9f804321ef5a40ab840e40c5ce8a7d1d
James Toledo <jamesmacena14@gmail.com>
2022-05-17 15:45:48 -0300

    test: Adm_User sees the start page and its content

---
+++ spec/system/admin_user_view_start_page_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+describe 'Admin_User visit the start page' do
+  it 'and sees the content' do
+    company =  create(:company)
+
+    visit company_index_path
+
+    expect(page).to have_content 'Gerenciamento de Transportadoras'
+    expect(page).to have_content company.brand_name
+    expect(page).to have_content company.state_abbr
+  end
+end
