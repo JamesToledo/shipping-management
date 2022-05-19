ce8d91736cb3083de28ca193994c2c63da03ebeb
James Toledo <jamesmacena14@gmail.com>
2022-05-19 12:16:27 -0300

    done: admin user successfully register a company

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -8,4 +8,21 @@ class CompanyController < ApplicationController
   def new
     @company = Company.new
   end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to company_index_path, notice: 'Transportadora Cadastrada!'
+    else
+      flash.now[:notice] = 'Transportadora não cadastrada'
+      render 'new'
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:brand_name, :corporate_name, :address,
+                                    :email_domain, :postal_code, :state_abbr)
+  end
 end
