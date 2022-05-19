5d0862a42e2c4a77c665ed3d4c47f9dfc420f1a2
James Toledo <jamesmacena14@gmail.com>
2022-05-18 21:17:47 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@
 
 Company.create!(brand_name: 'Apple', corporate_name: 'Apple Inc.', registration_number: '00623904000173',
                 email_domain: 'apple.com', address: 'Rua Leopoldo Couto de Magalhães Jr., 700',
-                postal_code: '04542-000')
+                postal_code: '04542-000', state_abbr: 'sc')
