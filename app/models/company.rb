8752f7dca9bd3b097eed8faa2c490020c43fd45f
James Toledo <jamesmacena14@gmail.com>
2022-05-19 18:02:43 -0300

    done: admin user receive feedback

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Company < ApplicationRecord
+  validates :brand_name, :corporate_name, :registration_number,
+            :address, :email_domain, :postal_code,
+            :state_abbr, presence: true
 end
