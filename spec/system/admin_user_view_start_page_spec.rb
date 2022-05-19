2cf781ddbc3b02e56d0b931e02e5ba4ddf42e48f
James Toledo <jamesmacena14@gmail.com>
2022-05-18 21:20:21 -0300

    test: feedback if there aren't companies registered

--- spec/system/admin_user_view_start_page_spec.rb
+++ spec/system/admin_user_view_start_page_spec.rb
@@ -10,4 +10,10 @@ describe 'Admin_User visit the start page' do
     expect(page).to have_content company.brand_name
     expect(page).to have_content company.state_abbr
   end
+
+  it 'and there are no companies registered' do
+    visit company_index_path
+
+    expect(page).to have_content 'não há transportadoras cadastradas'
+  end
 end
