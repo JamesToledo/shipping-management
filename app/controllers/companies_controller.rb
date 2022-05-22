79d9764f1d840dbe99a31f97558a172d376395b2
James Toledo <jamesmacena14@gmail.com>
2022-05-22 16:27:25 -0300

    done: admin user sees edit form
    also added: partial view and before action.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class CompaniesController < ApplicationController
+  before_action :set_company, only: %i[show edit]
+
   def index
     @companies = Company.all
   end
 
-  def show
-    @company = Company.find(params[:id])
-  end
+  def show; end
 
   def new
     @company = Company.new
@@ -25,10 +25,16 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit; end
+
   private
 
   def company_params
     params.require(:company).permit(:brand_name, :corporate_name, :registration_number, :address,
                                     :email_domain, :postal_code, :state_abbr)
+  end
+
+  def set_company
+    @company = Company.find(params[:id])
   end
 end
