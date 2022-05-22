732d44c36fb00ef1d15ede2b8d372b2ecdcaf0a9
James Toledo <jamesmacena14@gmail.com>
2022-05-22 16:04:25 -0300

    Test: admin user sees company details
    admin user see company details from admin homepage and can go back

---
+++ spec/system/admin_user_view_company_details_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'admin user sees company details' do
+  it 'from the admin home page' do
+    company = create(:company)
+
+    visit companies_path
+    click_on company.brand_name
+
+    expect(current_path).to eq(company_path(1))
+    expect(page).to have_css('h1', text: company.brand_name)
+    expect(page).to have_content "Razão Social: #{company.corporate_name}"
+    expect(page).to have_content "CNPJ: #{company.registration_number}"
+    expect(page).to have_content "Endereço: #{company.address} - #{
+                                  company.state_abbr}, #{company.postal_code}"
+  end
+
+  it 'and return to admin home page' do
+    company = create(:company)
+
+    visit companies_path
+    click_on company.brand_name
+    within 'nav' do
+      click_on 'voltar'
+    end
+    expect(current_path).to eq companies_path
+  end
+end
