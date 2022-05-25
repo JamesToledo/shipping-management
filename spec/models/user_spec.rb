8698e6763bc3119187e607442876f3f01f826067
James Toledo <jamesmacena14@gmail.com>
2022-05-25 10:25:17 -0300

    test: define user role by email domain

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'User authenticates' do
+    it 'as admin' do
+      admin = User.create!(username: 'admin', email: 'admin@sistemadefrete.com', password: 'Admin123')
+
+      expect(admin.role).to eq 'admin'
+    end
+
+    it 'as company_user' do
+      company = create(:company)
+      user = User.create!(username: 'Pedro', email: "pedro@#{company.email_domain}", password: 'Pedro123', role: 1)
+
+      expect(user.role).to eq 'company_user'
+    end
+  end
 end
