41153f8f8d351b771ae3a7d29039d7487ebf78c6
James Toledo <jamesmacena14@gmail.com>
2022-05-25 14:41:00 -0300

    rails sees update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
-Company.create!(brand_name: 'Apple', corporate_name: 'Apple Inc.', registration_number: '00623904000173',
-                email_domain: 'apple.com', address: 'Rua Leopoldo Couto de Magalhães Jr., 700',
-                postal_code: '04542-000', state_abbr: 'sc')
+company = Company.create!(brand_name: 'Apple', corporate_name: 'Apple Inc.', registration_number: '00623904000173',
+                          email_domain: 'apple.com', address: 'Rua Leopoldo Couto de Magalhães Jr., 700',
+                          postal_code: '04542-000', state_abbr: 'sc')
+
+User.create!(username: 'Pedrinho', email: "pedrinho@#{company.email_domain}", password: 'Pedrinho123')
+
+User.create!(username: 'Paula', email: 'paula@sistemadefrete.com', password: 'Paula123')
+
+Vehicle.create!(vehicle_model: 'cargo 1317', vehicle_make: 'ford', vehicle_plate: 'EPP-5432',
+                year: '2010', load_capacity: 3000, company_id: company.id)
