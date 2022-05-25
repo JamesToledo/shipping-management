a5a7b8b0a7589bbf31f8c55259ed64e4b1690d17
James Toledo <jamesmacena14@gmail.com>
2022-05-25 10:25:30 -0300

    done: test: define user role by email do

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: { admin: 1, company_user: 0 }
+  validate :set_user_role_by_email
+
+  private
+
+  def set_user_role_by_email
+    if email.present? && email.end_with?('@sistemadefrete.com')
+      self.role = :admin
+    else
+      Company.all.each do |company|
+        return self.role = :company_user if email.include?(company.email_domain)
+      end
+    end
+  end
 end
