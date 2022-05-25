3cb13dba737c1ab9bd9ab9b25a769a2b6a161fef
James Toledo <jamesmacena14@gmail.com>
2022-05-25 10:21:20 -0300

    test: user successfully sign up and logout

---
+++ spec/system/user/user_sign_up_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'User sign up' do
+  it 'successfully' do
+    user  = build(:user)
+
+    visit root_path
+    click_on 'Login'
+    click_on 'Inscrever-se'
+    fill_in 'Nome de Usuário', with: user.username
+    fill_in 'E-mail', with: user.email
+    fill_in 'Senha', with: user.password
+    fill_in 'Confirme sua senha', with: user.password
+    click_on 'cadastrar'
+
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso'
+  end
+
+  it 'and successfully logout' do
+    user  = build(:user)
+
+    visit root_path
+    click_on 'Login'
+    click_on 'Inscrever-se'
+    fill_in 'Nome de Usuário', with: user.username
+    fill_in 'E-mail', with: user.email
+    fill_in 'Senha', with: user.password
+    fill_in 'Confirme sua senha', with: user.password
+    click_on 'cadastrar'
+    click_on 'Logout'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Logout efetuado com sucesso'
+  end
+end
