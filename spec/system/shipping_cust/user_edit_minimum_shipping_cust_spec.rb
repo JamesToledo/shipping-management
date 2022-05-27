16b17edfe63857d9934a8e7e2bd1655ffe5340e4
James Toledo <jamesmacena14@gmail.com>
2022-05-27 08:48:13 -0300

    test: User edit minimum shipping successfully cost

---
+++ spec/system/shipping_cust/user_edit_minimum_shipping_cust_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'User edit minimum shipping cost' do
+  it 'successfully' do
+    company = create(:company)
+    user = create(:user, email: "pedro@#{company.email_domain}", company_id: company.id)
+    create(:shipping_cust, company_id: company.id)
+
+    sign_in user
+    visit quotes_path
+    click_on 'Editar Valor'
+    fill_in 'Frete Mínimo', with: 11
+    click_on 'atualizar'
+
+    expect(page).to have_content 'Valor Salvo!'
+    expect(current_path).to eq quotes_path
+    expect(page).to have_content 'R$ 11.0'
+  end
+end
