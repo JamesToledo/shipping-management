0d6157e952f4aeebf9f737e731185adb32ae4b33
James Toledo <jamesmacena14@gmail.com>
2022-05-28 18:33:03 -0300

    test: Admin user consults shipping price successfully

--- spec/system/user/admin_user_consults_shipping_price_spec.rb
+++ spec/system/user/admin_user_consults_shipping_price_spec.rb
@@ -18,4 +18,24 @@ describe 'Admin user consults shipping price' do
     expect(page).to have_content 'Distância'
     expect(page).to have_button 'Consultar'
   end
+
+  it 'successfully' do
+    company = create(:company)
+    create(:shipping_cust, value: 10, company_id: company.id)
+    create(:budget, max_size: 0.500, max_weight: 15, range_price: 0.5, company_id: company.id)
+    create(:deadline, max_space: 100, days: 2, company_id: company.id)
+    admin = create(:user, email: 'admin@sistemadefrete.com')
+
+    sign_in admin
+    visit orders_path
+    fill_in 'Altura', with: 0.5
+    fill_in 'Largura', with: 0.2
+    fill_in 'Comprimento', with: 0.35
+    fill_in 'Peso', with: 12
+    fill_in 'Distância', with: 73
+    click_on 'Consultar'
+
+    expect(page).to have_css('h1', text: 'Transportadoras Disponíveis')
+    expect(page).to have_content("R$ 36.5 - #{company.brand_name} - 2 dias úteis")
+  end
 end
